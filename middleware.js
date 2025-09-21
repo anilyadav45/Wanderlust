9600d114bb65bdf7c1a7923f189b01c201abbde1
Anil Yadav <rebelanil885@gmail.com>
2025-09-21 09:07:35 +0530

    user requested based redirect using req.originalUrl saved to session then locals

--- middleware.js
+++ middleware.js
@@ -1,11 +1,29 @@
 
 const isLoggedIn = (req, res, next) => {
     console.log("user is " + req.user);//here we can see the user object from what passport is auth. 
+    //maniye koi user direct new , edit , delete route pe ja raha tha but wo login na hone ka karan usse login form mila so usne login kiya but wo /listings pe redirect ho gaya so that is not good UX for user mano koi important page pe ja raha tha wo waha pe hi redirect ho uske liye hum session me -- 1.user ki requested url req.path and req.originalUrl me store karwa denge so after login wo kaha jana chahta tha wo waha redirect wo sake naki ki same page jo hamne after login defualt set kiye he
+
+    //start that - we can see mthods of req console.log(req.path+req.originalUrl);
+    //we store redirect url in session so we don't check for every route if user is already logged in 
 
     if (!req.isAuthenticated()) {
+        //inside if me -  isliye user kar rahe he qku hame pata he agar user login nahi huwa tabhi to user requested  url aakar save hoga then we'll use to redirect after login jaha use jana thaa
+        req.session.reqUrl = req.originalUrl;//access it to user login
+        console.log("requested url is " + req.session.reqUrl);
         req.flash("error", "You must have logged in first");
-        return res.redirect("/login");
+        return res.redirect("/login");//will redirect to login page whereever we're using it it will authenticate the user if not middleware will stop here and redirect to login page if authenticated call next that means continue to route
     }
     next();//if logged in call next middleware or route to continue
 }
-module.exports = { isLoggedIn };//we can export multiple middleware here
+
+
+//middleware to save requested url in session to locals becauuse passport reset session after login so we need midW in between before login so that we get saved reqUlr which was stored in session before ps reset it afterlogin
+
+const saveReqUrl = (req, res, next) => {
+    if (req.session.reqUrl) {//sahi me user req hai session me tab nai save karbe otherwise no reason 
+        res.locals.reqUrl = req.session.reqUrl;
+    }
+    next();
+}
+
+module.exports = { isLoggedIn, saveReqUrl };//we can export multiple middleware here
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const wrapAsync = require('../utils/wrapAsync');
 const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const passport = require('passport');
+const { saveReqUrl } = require("../middleware.js");//before pasport we use it to save requested url in session to locals after login ps reset session -- tahi par ete kare parihai
 
 //sign up routes
 router.get("/signup", (req, res) => {
@@ -14,9 +15,13 @@ router.post("/signup", wrapAsync(async (req, res) => {
         let { username, email, password } = req.body;//destructuring name should be same as in form
         const newUser = new User({ username, email });//creating new user instance password not passed here as it will be hashed by passport local mongoose
         let registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.flash("success", "Welcome to WanderLust");
-        res.redirect("/listings");
+        console.log(registeredUser);//jaise hi user registered ho jata hai usse login karwa do using pasport login method use karke
+        req.login(registeredUser, (err) => {
+            if (err) return next(err);
+            req.flash("success", "Welcome to WanderLust");
+            res.redirect("/listings");
+        })
+
     }
     catch (e) {
         req.flash("error", e.message);
@@ -24,32 +29,35 @@ router.post("/signup", wrapAsync(async (req, res) => {
     }
 }))
 
+
 //login routes
 router.get("/login", (req, res) => {
     res.render("./users/login.ejs");
 })
 
-router.post("/login",
-    passport.authenticate( //see docs for passport
+router.post("/login", saveReqUrl,
+    passport.authenticate( //see docs for passport--this is middleware between route and callback
         "local", { failureRedirect: '/login', failureFlash: true }
     ),
     //this only execute if papport middleware authenticated 
     wrapAsync(async (req, res) => {
         req.flash("success", "Login succesfully");
-        res.redirect("/listings");
+        //after login redirect to user requested url which use saved in session 
+        const redirectUrl = res.locals.reqUrl;
+        res.redirect(redirectUrl);
     })
 
 )
 
 // log out route : passport auto logout the user this is inbuild method 
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
             return next(err);
         }
-        req.flash("success","Logged out succesfully");
+        req.flash("success", "Logged out succesfully");
         res.redirect("/listings");
     })
 })
 
-    module.exports = router;
+module.exports = router;
