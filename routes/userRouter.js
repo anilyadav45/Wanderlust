52d69b8fe9092bbfe5f88195295008c4634dee3d
Anil Yadav <rebelanil885@gmail.com>
2025-10-04 23:19:28 +0530

    controller - MVC project Models,Views,Controllers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,21 +6,20 @@ const passport = require('passport');
 const { saveReqUrl } = require("../middleware.js");//before pasport we use it to save requested url in session to locals after login ps reset session -- tahi par ete kare parihai
 const userController = require("../controllers/user.js");
 
-//sign up routes
-router.get("/signup", userController.getSignUp);
-
-router.post("/signup", wrapAsync(userController.postSignUp));
-
+//signup common
+router.route("/signup")
+    .get(userController.getSignUp)
+    .post(wrapAsync(userController.postSignUp));
 
 //login routes
-router.get("/login", userController.getLogin);
-
-router.post("/login", saveReqUrl,
-    passport.authenticate( //see docs for passport--this is middleware between route and callback
-        "local", { failureRedirect: '/login', failureFlash: true }
-    ),
-    //this only execute if papport middleware authenticated 
-    wrapAsync(userController.postLogin));
+router.route("/login")
+    .get(userController.getLogin)
+    .post(saveReqUrl,
+        passport.authenticate( //see docs for passport--this is middleware between route and callback
+            "local", { failureRedirect: '/login', failureFlash: true }
+        ),
+        //this only execute if papport middleware authenticated 
+        wrapAsync(userController.postLogin));
 
 // log out route : passport auto logout the user this is inbuild method 
 router.get("/logout", userController.logOut);
