0590f30d55b417e7e005ab6ead5c2146b690be8d
Anil Yadav <anil@Anils-MacBook-Air.local>
2025-11-05 19:08:25 -0800

    search btn styles

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,19 @@
 //this is for where ever we use bootstrap validation .need-validation class -- for user side validation
 (() => {
-        "use strict";
-        const forms = document.querySelectorAll(".needs-validation");
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
 
-        Array.from(forms).forEach((form) => {
-          form.addEventListener(
-            "submit",
-            (event) => {
-              if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add("was-validated");
-            },
-            false
-          );
-        });
-      })();
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
